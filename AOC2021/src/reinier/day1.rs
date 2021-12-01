9af6eac2b16eb11e86229a439ba256f708e7feac
Reinier van der Horst <reiniervanderhorst@outlook.com>
2021-12-01 15:24:16 +0100

    basic file structure

---
+++ AOC2021/src/reinier/day1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("hoi");
+}
+
+fn puzzle1() {
+    println!("puzzle1");
+}
+
+
+fn puzzle2() {
+    println!("puzzle2");
+}
---
+++ AOC2021/src/reinier/tools.rs
@@ -0,0 +1,9 @@
+mod tools;
+
+import std::fs;
+
+fn read_input(day :u32, challenge :u32) -> String {
+    const file_loc = "src/reinier/input/day{:?}_{:?}".format(day, challenge);
+    
+    fs::read_to_string()
+}
