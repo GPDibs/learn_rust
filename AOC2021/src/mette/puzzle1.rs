2721aada0484829b9302722d8e9b6f66fa6dba2f
Mette Weisfelt <metteweisfelt@gmail.com>
2021-12-01 14:57:24 +0100

    day one input file and puzzle file

---
+++ AOC2021/src/mette/puzzle1.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() {
+    let file = File::open("inputp1p2.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut count:u32 = 0;
+    let mut previous: u32 = 0;
+
+    for line in reader.lines(){
+        let current:u32 = line.expect("error current").trim().parse().unwrap();      
+
+        if current > previous {
+            count = count + 1;
+        }
+
+        // update previous
+        previous = current;
+    }
+
+    let total = count - 1; // minus first one
+    println!("{}", total);
+    
+}
